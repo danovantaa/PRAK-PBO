73b415cf14e74d09bcf28083cc549fc7f20495cf
danovantaa <zefanyadanovantaa@gmail.com>
2024-03-01 16:52:34 +0700

    Rename

---
+++ pertemuan1/122140101_Zefanya Danovanta Tarigan_Prak1_Soal2.py
@@ -0,0 +1,12 @@
+R = float(input("Jari - Jari : "))
+pi = 3.14
+Luas = R**2 * pi
+Keliling = 2 * pi * R
+
+if R <= 0 : 
+    print("Jari Jari Tidak boleh negatif ")
+
+else : 
+    print(f"Luas : {Luas}")
+    print(f"Keliling : {Keliling}")
+
